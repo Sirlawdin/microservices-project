0255f4ab0d9579a75af3cc62c30f21e539d9778d
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-08-20 08:40:55 +0100

    Added Screenshot

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a462bd4c3cad0470c81d9a150457df7f-1412705385.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afa0e647a4be54d09a31a73d18b6eb8a-1264420628.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
